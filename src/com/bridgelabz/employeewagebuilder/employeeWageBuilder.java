a1780659df44e4c9b396588726eab7101aed9e5f
Prashanth <imprash4u@gmail.com>
2021-08-31 22:47:22 +0530

    Added:Part time work hour

--- src/com/bridgelabz/employeewagebuilder/employeeWageBuilder.java
+++ src/com/bridgelabz/employeewagebuilder/employeeWageBuilder.java
@@ -5,17 +5,24 @@ import java.util.Random;
 public class employeeWageBuilder {
     static final int WAGEPERHR = 20;
     static final int FULLDAYHR = 8;
+    static final int PARTTIMEHR = 4;
+
 
     public static void main(String[] args) {
         System.out.println("......Welcome to Employee Wage Builder Program.....");
         Random random = new Random();
-        int empCheck = random.nextInt(2);
+        int dailyEmpWage;
+        int empCheck = random.nextInt(3);
         if (empCheck == 0) {
-            System.out.println("Employee is present for work ");
-            int dailyEmpWage = WAGEPERHR * FULLDAYHR;
-            System.out.println("Employee daily wage: "+dailyEmpWage);
+            System.out.println("Employee is present for work full day ");
+            dailyEmpWage = WAGEPERHR * FULLDAYHR;
+            System.out.println("Employee daily wage: " + dailyEmpWage);
+        } else if (empCheck == 1) {
+            System.out.println("Employee present for Halfday (part time)");
+            dailyEmpWage = WAGEPERHR * PARTTIMEHR;
+            System.out.println("Employee wage: "+dailyEmpWage);
         } else {
-            System.out.println("employee Absent for work");
+            System.out.println("Employee Absent for work");
         }
     }
 }
