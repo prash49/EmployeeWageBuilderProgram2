ab92bced7662da9149bdcb9a0d837dc7cdaefbac
Prashanth <imprash4u@gmail.com>
2021-08-31 22:54:35 +0530

    Added:Code refacored using Switch case

--- src/com/bridgelabz/employeewagebuilder/employeeWageBuilder.java
+++ src/com/bridgelabz/employeewagebuilder/employeeWageBuilder.java
@@ -12,17 +12,20 @@ public class employeeWageBuilder {
         System.out.println("......Welcome to Employee Wage Builder Program.....");
         Random random = new Random();
         int dailyEmpWage;
-        int empCheck = random.nextInt(3);
-        if (empCheck == 0) {
-            System.out.println("Employee is present for work full day ");
-            dailyEmpWage = WAGEPERHR * FULLDAYHR;
-            System.out.println("Employee daily wage: " + dailyEmpWage);
-        } else if (empCheck == 1) {
-            System.out.println("Employee present for Halfday (part time)");
-            dailyEmpWage = WAGEPERHR * PARTTIMEHR;
-            System.out.println("Employee wage: "+dailyEmpWage);
-        } else {
-            System.out.println("Employee Absent for work");
+        double empCheck = (Math.floor(Math.random() * 10) % 3);
+        switch ((int) empCheck) {
+            case 0:
+                System.out.println("Employee is present for work full day ");
+                dailyEmpWage = WAGEPERHR * FULLDAYHR;
+                System.out.println("Employee daily wage: " + dailyEmpWage);
+                break;
+            case 1:
+                System.out.println("Employee present for Halfday (part time)");
+                dailyEmpWage = WAGEPERHR * PARTTIMEHR;
+                System.out.println("Employee wage: " + dailyEmpWage);
+                break;
+            case 2:
+                System.out.println("Employee Absent for work");
         }
     }
 }
