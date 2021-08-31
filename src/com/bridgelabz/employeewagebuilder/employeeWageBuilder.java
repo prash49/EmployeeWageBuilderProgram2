d6e27ad08c1a9bca423314fe8e2cd9033ec679de
Prashanth <imprash4u@gmail.com>
2021-08-31 23:24:03 +0530

    Added: check emp Wage for max 100hr or 20days

--- src/com/bridgelabz/employeewagebuilder/employeeWageBuilder.java
+++ src/com/bridgelabz/employeewagebuilder/employeeWageBuilder.java
@@ -7,6 +7,7 @@ public class employeeWageBuilder {
     static final int FULL_DAY_HR = 8;
     static final int PART_TIME_HR = 4;
     static final int MAX_WORKING_DAYS = 20;
+    static final int MAX_HRS_PER_MONTH = 100;
 
 
     public static void main(String[] args) {
@@ -14,25 +15,33 @@ public class employeeWageBuilder {
         Random random = new Random();
         int dailyEmpWage;
         int toalWage = 0;
-        for (int i = 1; i <= 20; i++) {
+        int totalWorkingHrs=0;
+        int totalWorkingDays=0;
+
+        while (totalWorkingHrs <= MAX_HRS_PER_MONTH && totalWorkingDays < MAX_WORKING_DAYS){
+            totalWorkingDays++;
             double empCheck = (Math.floor(Math.random() * 10) % 3);
             switch ((int) empCheck) {
                 case 0:
                     System.out.println("Employee is present for work full day ");
                     dailyEmpWage = WAGE_PER_HR * FULL_DAY_HR;
-                    System.out.println("Employee daily wage on day  " + i + " is :" + dailyEmpWage);
+                    System.out.println("Employee daily wage on day is :" + dailyEmpWage);
                     toalWage += dailyEmpWage;
+                    totalWorkingHrs += FULL_DAY_HR;
                     break;
                 case 1:
                     System.out.println("Employee present for Halfday (part time)");
                     dailyEmpWage = WAGE_PER_HR * PART_TIME_HR;
-                    System.out.println("Employee wage on day: " + i + " is :" + dailyEmpWage);
+                    System.out.println("Employee wage is :" + dailyEmpWage);
                     toalWage += dailyEmpWage;
+                    totalWorkingHrs += PART_TIME_HR;
                     break;
                 case 2:
-                    System.out.println("Employee Absent for work on day: "+i);
+                    System.out.println("Employee Absent for work on day");
             }
-            System.out.println("Total Employee Wage for 20 is : " + toalWage);
+            System.out.println("Total Employee Wage  : " + toalWage);
+            System.out.println("Number of Working Days:"+totalWorkingDays);
+            System.out.println("Number of Working Hours:"+totalWorkingHrs);
         }
     }
 }
