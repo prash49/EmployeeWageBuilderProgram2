827362736ecf794dd0b914db2d97cf52f78778c5
Prashanth <imprash4u@gmail.com>
2021-08-31 23:11:14 +0530

    Added: Calculating monthly wage for 20days

--- src/com/bridgelabz/employeewagebuilder/employeeWageBuilder.java
+++ src/com/bridgelabz/employeewagebuilder/employeeWageBuilder.java
@@ -3,29 +3,36 @@ package com.bridgelabz.employeewagebuilder;
 import java.util.Random;
 
 public class employeeWageBuilder {
-    static final int WAGEPERHR = 20;
-    static final int FULLDAYHR = 8;
-    static final int PARTTIMEHR = 4;
+    static final int WAGE_PER_HR = 20;
+    static final int FULL_DAY_HR = 8;
+    static final int PART_TIME_HR = 4;
+    static final int MAX_WORKING_DAYS = 20;
 
 
     public static void main(String[] args) {
         System.out.println("......Welcome to Employee Wage Builder Program.....");
         Random random = new Random();
         int dailyEmpWage;
-        double empCheck = (Math.floor(Math.random() * 10) % 3);
-        switch ((int) empCheck) {
-            case 0:
-                System.out.println("Employee is present for work full day ");
-                dailyEmpWage = WAGEPERHR * FULLDAYHR;
-                System.out.println("Employee daily wage: " + dailyEmpWage);
-                break;
-            case 1:
-                System.out.println("Employee present for Halfday (part time)");
-                dailyEmpWage = WAGEPERHR * PARTTIMEHR;
-                System.out.println("Employee wage: " + dailyEmpWage);
-                break;
-            case 2:
-                System.out.println("Employee Absent for work");
+        int toalWage = 0;
+        for (int i = 1; i <= 20; i++) {
+            double empCheck = (Math.floor(Math.random() * 10) % 3);
+            switch ((int) empCheck) {
+                case 0:
+                    System.out.println("Employee is present for work full day ");
+                    dailyEmpWage = WAGE_PER_HR * FULL_DAY_HR;
+                    System.out.println("Employee daily wage on day  " + i + " is :" + dailyEmpWage);
+                    toalWage += dailyEmpWage;
+                    break;
+                case 1:
+                    System.out.println("Employee present for Halfday (part time)");
+                    dailyEmpWage = WAGE_PER_HR * PART_TIME_HR;
+                    System.out.println("Employee wage on day: " + i + " is :" + dailyEmpWage);
+                    toalWage += dailyEmpWage;
+                    break;
+                case 2:
+                    System.out.println("Employee Absent for work on day: "+i);
+            }
+            System.out.println("Total Employee Wage for 20 is : " + toalWage);
         }
     }
 }
